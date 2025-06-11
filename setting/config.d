462a0cb59384ca531d2b71243d38afa7a78f55a3
hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>
2025-06-11 20:27:13 +0500

    Update config.d

--- setting/config.d
+++ setting/config.d
@@ -1,4 +1,4 @@
-#Touchpad Settings
+
 input "type:touchpad" {
 tap enabled       #enables click-on-tap
 
@@ -8,11 +8,8 @@ input * {
 xkb_layout us,ru
 xkb_options grp:alt_shift_toggle
 }
-
-#Screenshots
-#$mod+PrintScreen - arbitrary part
-#PrintScreen - whole screen
-  
-bindsym Print exec grim ~/Pictures/screen-"$(date +%s)".png
-bindsym $mod+Print exec grim -g "$(slurp)" ~/Pictures/screen-"$(date +%s)".png
-}
+bindsym XF86AudioRaiseVolume exec pactl set-sink-volume @DEFAULT_SINK@ +5% && $refresh_display
+bindsym XF86AudioLowerVolume exec pactl set-sink-volume @DEFAULT_SINK@ -5% && $refresh_display
+bindsym XF86AudioMute exec pactl set-sink-mute @DEFAULT_SINK@ toggle && $refresh_display
+bindsym XF86MonBrightnessUp exec brightnessctl set +5%
+bindsym XF86MonBrightnessDown exec brightnessctl set- 5%
