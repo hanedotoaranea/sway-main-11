67cfc31fd67b5cad3d9dce30a74bdce08753e61f
hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>
2025-06-11 20:34:08 +0500

    Update config.d

--- setting/config.d
+++ setting/config.d
@@ -10,6 +10,6 @@ xkb_options grp:alt_shift_toggle
 }
 bindsym XF86AudioRaiseVolume exec pactl set-sink-volume @DEFAULT_SINK@ +5% && $refresh_display
 bindsym XF86AudioLowerVolume exec pactl set-sink-volume @DEFAULT_SINK@ -5% && $refresh_display
-bindsym XF86AudioMute exec pactl set-sink-mute @DEFAULT_SINK@ toggle && $refresh_display
-bindsym XF86MonBrightnessUp exec brightnessctl set +5%
-bindsym XF86MonBrightnessDown exec brightnessctl set -5%
+
+bindsym XF86MonBrightnessUp exec brightnessctl set +5% && notify-send "Яркость: $(brightnessctl get)%"
+bindsym XF86MonBrightnessDown exec brightnessctl set 5%- && notify-send "Яркость: $(brightnessctl get)%"
